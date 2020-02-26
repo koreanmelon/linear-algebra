8906eed9e6464348892474c78ca30689c7733022
Taewon Kim <taewon@berkeley.edu>
2020-02-26 06:44:57 -0800

    Small additions

--- Matrix.js
+++ Matrix.js
@@ -9,6 +9,20 @@ export class Matrix extends Vector {
     constructor(matrixArr) {
         super(...matrixArr);
         this._rows = matrixArr;
+        this._linear = [];
+        for (let j = 0; j < matrixArr.length; j += 1) {
+            for (let i = 0; i < matrixArr[0].length; i += 1) {
+                this._linear.push(matrixArr[i][j]);
+            }
+        }
+    }
+
+    /**
+     * Getter for the _rows property.
+     * @returns {Number[][]}
+     */
+    get rows() {
+        return this._rows;
     }
 
     /**
@@ -27,6 +41,6 @@ export class Matrix extends Vector {
         }
         return res.trimRight();
     }
-
 }
-
+let A = new Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+console.log(A.toString());
